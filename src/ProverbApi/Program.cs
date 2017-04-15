b4957225bbb28c6d667aedb7d357d785cf543599
John Reilly <johnny_reilly@hotmail.com>
2017-04-15 08:07:52 +0100

    align debugging directory with dotnet run

--- src/ProverbApi/Program.cs
+++ src/ProverbApi/Program.cs
@@ -10,8 +10,7 @@ namespace Proverb.Api.Core
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
-                // .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseContentRoot(System.IO.Path.Combine(Directory.GetCurrentDirectory(), "src/ProverbApi/"))
+                .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
