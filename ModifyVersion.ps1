882c08534730525a97634aa5c492e1405f3ad1fc
John Reilly <johnny_reilly@hotmail.com>
2017-04-16 20:14:09 +0100

    set version of application using AppVeyor build number

--- ModifyVersion.ps1
+++ ModifyVersion.ps1
@@ -1,28 +1,29 @@
-Param($projectFile, $buildNum, $buildVersion)
+Param($projectFile, $buildNum)
 
-write-host "project file: $projectFile"
-write-host "build num: $buildNum"
-write-host "build version: $buildVersion"
+# write-host "project file: $projectFile"
+# write-host "build num: $buildNum"
+
+$content = [IO.File]::ReadAllText($projectFile)
 
 $regex = new-object System.Text.RegularExpressions.Regex ('(<Version>)([\d]+.[\d]+.[\d]+)(.[\d]+)(<\/Version>)', 
          [System.Text.RegularExpressions.RegexOptions]::MultiLine)
 
-$content = [IO.File]::ReadAllText($projectFile)
-
 $version = $null
 $match = $regex.Match($content)
 if($match.Success) {
+    # from "<Version>1.0.0.0</Version>" this will extract "1.0.0"
     $version = $match.groups[2].value
 }
 
-# new version
+# suffix build number onto $version. eg "1.0.0.15"
 $version = "$version.$buildNum"
 
-# update assembly info
+# update "<Version>1.0.0.0</Version>" to "<Version>$version</Version>"
 $content = $regex.Replace($content, '${1}' + $version + '${4}')
 
+# update csproj file
 write-host $content
-#[IO.File]::WriteAllText($projectFile, $content)
+[IO.File]::WriteAllText($projectFile, $content)
 
 # update AppVeyor build
-#Update-AppveyorBuild -Version $version
+Update-AppveyorBuild -Version $version
